b8153048ddc979ff8b15688728b2a7615a5c6ea0
megandowling <neuro7plastic@yahoo.com>
2019-08-30 14:53:31 -0700

    added gifs

--- script.js
+++ script.js
@@ -23,10 +23,17 @@ function myFunction() {
 
 var backgroundImg = 1;
 var images = [
-  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT961wvpSl2f-3xbAS3RwCW1fwuLNfePAsvxWiJC25AaayyF_Nb",
-"https://images2.minutemediacdn.com/image/upload/c_crop,h_1414,w_2101,x_20,y_0/v1565279671/shape/mentalfloss/578211-gettyimages-542930526.jpg?itok=Nj49PDxW"
+  "https://i.ytimg.com/vi/DJMkJybnoQI/maxresdefault.jpg",
+  "https://media3.giphy.com/media/3og0INtldac8gncQO4/giphy.gif?cid=790b7611c2a97130dc9864ffd1607d59ef5f77805707d57e&rid=giphy.gif",
+  "https://images.immediate.co.uk/production/volatile/sites/4/2018/10/p1501ay_0-4a31e22.jpg?quality=90&resize=620,413",
+  "https://media2.giphy.com/media/3ov9jWPi1AH2XUfI6Q/giphy.gif?cid=790b761160408f09823f70c3ef0cf2a83f836ba62695d07e&rid=giphy.gif",
+  "https://i.ytimg.com/vi/62XtuOMndxM/maxresdefault.jpg",
+  "https://media0.giphy.com/media/l3c614V12UA82q1vG/giphy.gif?cid=790b761160408f09823f70c3ef0cf2a83f836ba62695d07e&rid=giphy.gif",
+  "https://images.immediate.co.uk/production/volatile/sites/4/2018/10/heic1520a_1280-d0693f4.jpg?quality=45&resize=960,413",
+  "http://33.media.tumblr.com/ca21bf60fd701a2aeb84def658fa0767/tumblr_nn50a10eHS1tejz4lo1_540.gif"
 ]
-setInterval(function(){
-  $("#intro").css("background-image", `url(${images[backgroundImg%2]})`)
+
+setInterval(function () {
+  $("#intro").css("background-image", `url(${images[backgroundImg % 8]})`)
   backgroundImg++
-},5000)
+}, 5000)
